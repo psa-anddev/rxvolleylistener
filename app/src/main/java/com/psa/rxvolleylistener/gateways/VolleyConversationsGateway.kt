de13ef89aac12b5388d55602610da468d67f2d97
Pablo Sánchez <pablo.sanchez.alonso@gmail.com>
2017-11-04 17:29:58 +0000

    Clean up.

--- app/src/main/java/com/psa/rxvolleylistener/gateways/VolleyConversationsGateway.kt
+++ app/src/main/java/com/psa/rxvolleylistener/gateways/VolleyConversationsGateway.kt
@@ -1,10 +1,8 @@
 package com.psa.rxvolleylistener.gateways
 
-import android.content.Context
 import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.toolbox.JsonArrayRequest
-import com.android.volley.toolbox.Volley
 import com.psa.rxvolleylistener.RxVolleyListener
 import com.psa.rxvolleylistener.entities.Conversation
 import io.reactivex.Observable
